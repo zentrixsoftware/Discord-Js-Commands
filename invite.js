e7070bdb05f457caf9d380fc1d8ee7d5c2407eb6
zentrixsoftware <37421653+zentrixsoftware@users.noreply.github.com>
2018-03-16 23:50:27 +0000

    Update invite.js

--- invite.js
+++ invite.js
@@ -1,35 +1,37 @@
+/*
+This command basicly a fancy way to tell users how they can add your bot
+check at the bottom to see how it looks
+*/
+
 const { RichEmbed } = require('discord.js');
-const Discors = require('discord.js');
+const config = require('../config/config.json');
+exports.run = async (client, msg, args) => {
 
-exports.run = async (client, args, msg) => {
-//Yet again im using "Method" but you can still use "Properties or Value" 
-
-const embed = new RichEmbed()
-.setAuthor(msg.author.username) //Show username of the person who executed command 
+const embed = new RichEmbed() // Discord embed constructor 
+.setAuthor(msg.author.username , msg.author.avatarURL) //Show username and profile pic of the person who executed command 
 .setColor(Math.floor(Math.random()*16777215)) //Random embed color 
-.addField(Invite Bot:, 'Hey feel free to invite my bot to your server')
-.addField('learners bot', '[Invite bot](Your-0auth2-link')) //This "[]()" is basicly hyperlinking a text or word 
+.addField('Invite Bot:' , 'Hey feel free to invite my bot to your server')
+.addField('learners bot', '[Invite bot](Your-0auth2-link') //This "[]()" is basicly hyperlinking a text or word 
 .setTimestamp(); //Stamps new time 
 
-//Sends the embed in the channel command was executed
-msg.channel.send({embed})
+msg.channel.send({embed}) //Send the embed to the channel the command was executed
+ 
+ //msg.author.send({embed})
+// You can also use this to get the bot to send it to the dms
 
-
-//You can use this to send to users dm when they use the command
-msg.author.send({embed})
-
+ 
+ //This part will be diffrent based on your bot..
 };
-//This is for my bot your will be diffrent
-exports.help = { 
-category: 'util', 
-usage: 'invite', 
-description: 'invitation to invite bot', 
-detail: 'embeds messages',
- botPerm: ['SEND_MESSAGES'], authorPerm: [], 
- alias: ['add'], 
- example: 'invite'
+exports.help = {
+    category: 'util',
+    usage: 'invite',
+    description: 'sends information on how to add bot to your server',
+    detail: 'Embed Invitation',
+    botPerm: ['SEND_MESSAGES'],
+    authorPerm: [],
+    alias: [ ],
+    example: 'invite'
+};
 
- };
 
-
-//I will upload url to show example 
+//Preview : http://prntscr.com/isa7zq
